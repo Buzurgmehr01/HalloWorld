782a6028dc96e455a513faebe39e2c71b261a57b
Buuzurgmehr01 <majidovbuzurgmehr70@gmail.com>
2023-01-08 12:32:26 +0500

    Add  some func in button

--- HalloWorld/ViewController.swift
+++ HalloWorld/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var showButton: UIButton!
+    @IBOutlet weak var halloLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        halloLabel.isHidden = true
+        showButton.layer.cornerRadius = 10
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func makeButtonAction(_ sender: Any) {
+        if halloLabel.isHidden{
+            halloLabel.isHidden = false
+            showButton.setTitle("Hide text", for: .normal)
+        }else{
+            halloLabel.isHidden = true
+            showButton.setTitle("Show Text", for: .normal)
+        }
+    }
+    
 }
 
